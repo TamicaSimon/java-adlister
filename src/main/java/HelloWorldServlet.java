1fe973e3480b731dd04f03e8c028f3a5dd82b694
Tamica Simon <tamica.simon@gmail.com>
2023-05-01 21:43:58 +0000

    Hello World Servlets

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,30 @@
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello World!</h1>");
     }
 }
+
+
+//Create a page that displays "Hello World!" when a user visits /hello
+//Bonus
+//
+//Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
+//
+//Create a page view counter
+//
+//Create a page that displays a number that goes up by one every time the /count page is viewed.
+//
+//Bonus
+//
+//Allow the user to pass a parameter in the query string to reset the counter.
